6f628cec5eb7a957e1f2c9eddce343590037c337
Anderson Porto <anderson.higo2@gmail.com>
2023-01-18 02:33:18 -0300

    add: cors support

--- app/main.py
+++ app/main.py
@@ -3,10 +3,20 @@ from fastapi import FastAPI
 from app import models
 from app.config import engine
 from app.routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=engine)
 
+origins = ["*"]
+
 app = FastAPI()
 
-app.include_router(router, prefix="/api/v1", tags=["accounts"])
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+app.include_router(router, prefix="/api/v1", tags=["accounts"])
