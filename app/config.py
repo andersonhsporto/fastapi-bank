9764a2d021607b6636647887e15cf7451fdc8662
Anderson Porto <anderson.higo2@gmail.com>
2023-01-18 17:09:44 -0300

    fix: read slq script

--- app/config.py
+++ app/config.py
@@ -1,13 +1,24 @@
-import importlib
-
-from sqlalchemy import create_engine, inspect, MetaData
-from sqlalchemy.orm import sessionmaker
+import sqlparse as sqlparse
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/postgres"
+# DATABASE_URL = "mariadb+mariadbconnector://mariadb:mariadb@localhost:3306/mariadb"
+DATABASE_URL = "mysql+pymysql://mariadb:secret@127.0.0.1:3306/mariadb"
 
 engine = create_engine(DATABASE_URL)
+
+with open("./resources/data.sql") as file:
+    sql_raw = file.read()
+
+sql_queries = sqlparse.split(
+    sqlparse.format(sql_raw, strip_comments=True)
+)
+
+with engine.connect() as conn:
+    for query in sql_queries:
+        result = conn.execute(text(query))
+        print(f"{result.rowcount} rows have been updated/selected.")
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
