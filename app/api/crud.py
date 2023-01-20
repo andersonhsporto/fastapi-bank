0ce9a68fa016b0d491fe34d3524e4b5f0051b43f
Anderson Porto <anderson.higo2@gmail.com>
2023-01-20 02:12:19 -0300

    add: main get

--- app/api/crud.py
+++ app/api/crud.py
@@ -1,11 +1,38 @@
 from sqlalchemy.orm import Session
 
+from app.models.accountDto import AccountDto
 from app.models.models import Account, Transfer
 from app.schema.schemas import AccountSchema
 
 
-def get_account(db: Session, skip: int = 0, limit: int = 100):
+def get_all_account(db: Session, skip: int = 0, limit: int = 100):
     return db.query(Account).offset(skip).limit(limit).all()
+
+
+def get_account(db: Session,
+                account_id: int,
+                page: int,
+                size: int,
+                initial_date: str,
+                final_date: str,
+                operator_name: str):
+    account_dto = AccountDto(
+        id=account_id,
+        name=None,
+        page=page,
+        size=size,
+        initial_date=initial_date,
+        final_date=final_date,
+        operator_name=operator_name
+    )
+    if account_dto.is_valid_name_and_date():
+        return _account_by_name_and_date(db, account_dto)
+    elif account_dto.is_valid_date():
+        return _account_by_date(db, account_dto)
+    elif account_dto.is_valid_operator_name():
+        return _account_by_operator_name(db, account_dto)
+    else:
+        return _account_by_id(db, account_dto)
 
 
 def get_book_by_id(db: Session, book_id: int):
@@ -39,3 +66,32 @@ def update_account(db: Session, account: Account):
 
 def get_transfers(db: Session, skip: int = 0, limit: int = 100):
     return db.query(Transfer).offset(skip).limit(limit).all()
+
+
+def _account_by_name_and_date(db: Session, account_dto: AccountDto):
+    return db.query(Transfer)\
+            .filter(Transfer.account_id == account_dto.id)\
+            .filter(Transfer.operator_name == account_dto.operator_name)\
+            .filter(Transfer.date.between(account_dto.initial_date, account_dto.final_date))\
+            .offset(account_dto.page).limit(account_dto.size).all()
+
+
+def _account_by_date(db: Session, account_dto: AccountDto):
+    return db.query(Transfer)\
+            .filter(Transfer.account_id == account_dto.id)\
+            .filter(Transfer.date.between(account_dto.initial_date, account_dto.final_date))\
+            .offset(account_dto.page).limit(account_dto.size).all()
+
+
+def _account_by_operator_name(db: Session, account_dto: AccountDto):
+    return db.query(Transfer)\
+            .filter(Transfer.account_id == account_dto.id)\
+            .filter(Transfer.operator_name == account_dto.operator_name)\
+            .offset(account_dto.page).limit(account_dto.size).all()
+
+
+def _account_by_id(db: Session, account_dto: AccountDto):
+    return db.query(Transfer)\
+            .filter(Transfer.account_id == account_dto.id)\
+            .offset(account_dto.page).limit(account_dto.size).all()
+
--- app/api/routes.py
+++ app/api/routes.py
@@ -17,9 +17,23 @@ def get_db():
         db.close()
 
 
-@router.get("/accounts")
-async def get_accounts(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+@router.get("/accounts/all")
+async def get_all_account(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     _account = crud.get_account(db, skip, limit)
+    return Response(status="OK", code=200, message="Success", result=_account)
+
+
+@router.get("/accounts")
+async def get_account_by_id(
+        id: int,
+        page: int,
+        size: int,
+        initialDate: str | None = None,
+        finalDate: str | None = None,
+        operatorName: str | None = None,
+        db: Session = Depends(get_db)
+):
+    _account = crud.get_account(db, id, page, size, initialDate, finalDate, operatorName)
     return Response(status="OK", code=200, message="Success", result=_account)
 
 
